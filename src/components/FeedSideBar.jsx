5a53c7b3188bd7a4d35079b036d214f25bb94824
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-02-02 20:00:51 +0200

    Fix Img URL

--- src/components/FeedSideBar.jsx
+++ src/components/FeedSideBar.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const FeedSideBar = () => {
   return (
     <div>
-      <div className="hidden sm:flex w-52 md:w-330 lg:w-360 justify-start items-center px-3 rounded-full border-none outline-none focus-within:shadow-sm text-gray_1 bg-searchBg">
+      <div className="flex justify-start items-center px-3 border-none outline-none focus-within:shadow-sm bg-white">
         <div className="inline-block"></div>
         <input
           type="search"
           name="search"
-          className="p-2 rounded-full md:w-360 w-190 outline-none bg-searchBg"
+          className="p-2 outline-none bg-white"
           placeholder="Search for your favorite groups in ATG"
         />
       </div>
